a4530d21650e732e2429f904877a446d4ccc4b73
Yerassyl Altay <altayyerassyl@gmail.com>
2024-10-01 18:00:00 +0200

    test

--- breach.sh
+++ breach.sh
@@ -1802,4 +1802,100 @@ else
   say "    ${e}url:${x} $url"
   say "    ${e}query:${x} $query"
 
-ls .
+  # Full query without token (to display on terminal output)
+  queryNoToken=$(echo "package=$package-$VERSION&token=secret&$query" | tr -d ' ')
+  # now add token to query
+  query=$(echo "package=$package-$VERSION&token=$token&$query" | tr -d ' ')
+
+  # if [ "$ft_s3" = "1" ];
+  # then
+  #   say "${e}->${x}  Pinging Codecov"
+  #   say "$url/upload/v4?$queryNoToken"
+  #   # shellcheck disable=SC2086,2090
+  #   res=$(curl $curl_s -X POST $cacert \
+  #         --retry 5 --retry-delay 2 --connect-timeout 2 \
+  #         -H 'X-Reduced-Redundancy: false' \
+  #         -H 'X-Content-Type: application/x-gzip' \
+  #         -H 'Content-Length: 0' \
+  #         --write-out "\n%{response_code}\n" \
+  #         $curlargs \
+  #         "$url/upload/v4?$query" || true)
+  #   # a good reply is "https://codecov.io" + "\n" + "https://storage.googleapis.com/codecov/..."
+  #   s3target=$(echo "$res" | sed -n 2p)
+  #   status=$(tail -n1 <<< "$res")
+
+  #   if [ "$status" = "200" ] && [ "$s3target" != "" ];
+  #   then
+  #     say "${e}->${x}  Uploading to"
+  #     say "${s3target}"
+
+  #     # shellcheck disable=SC2086
+  #     s3=$(curl -fiX PUT \
+  #         --data-binary @"$upload_file.gz" \
+  #         -H 'Content-Type: application/x-gzip' \
+  #         -H 'Content-Encoding: gzip' \
+  #         $curlawsargs \
+  #         "$s3target" || true)
+
+  #     if [ "$s3" != "" ];
+  #     then
+  #       say "    ${g}->${x} Reports have been successfully queued for processing at ${b}$(echo "$res" | sed -n 1p)${x}"
+  #       exit 0
+  #     else
+  #       say "    ${r}X>${x} Failed to upload"
+  #     fi
+  #   elif [ "$status" = "400" ];
+  #   then
+  #       # 400 Error
+  #       say "${r}${res}${x}"
+  #       exit ${exit_with}
+  #   else
+  #       say "${r}${res}${x}"
+  #   fi
+  # fi
+
+  # say "${e}==>${x} Uploading to Codecov"
+
+  # # shellcheck disable=SC2086,2090
+  # res=$(curl -X POST $cacert \
+  #       --data-binary @"$upload_file.gz" \
+  #       --retry 5 --retry-delay 2 --connect-timeout 2 \
+  #       -H 'Content-Type: text/plain' \
+  #       -H 'Content-Encoding: gzip' \
+  #       -H 'X-Content-Encoding: gzip' \
+  #       -H 'Accept: text/plain' \
+  #       $curlargs \
+  #       "$url/upload/v2?$query&attempt=$i" || echo 'HTTP 500')
+  # # HTTP 200
+  # # http://....
+  # status=$(echo "$res" | head -1 | cut -d' ' -f2)
+  # if [ "$status" = "" ] || [ "$status" = "200" ];
+  # then
+  #   say "    Reports have been successfully queued for processing at ${b}$(echo "$res" | head -2 | tail -1)${x}"
+  #   exit 0
+  # else
+  #   say "    ${g}${res}${x}"
+  #   exit ${exit_with}
+  # fi
+
+  # say "    ${r}X> Failed to upload coverage reports${x}"
+  result=$(./codecov --verbose upload-process --disable-search --fail-on-error \
+    -t ${CODECOV_TOKEN} \
+    -n 'service'-${RUN_ID} \
+    -F service \
+    -f coverage-service.xml 2>&1)  # Capture both stdout and stderr
+
+  # Check if the command was successful
+  if [ $? -ne 0 ]; then
+      # If there is an error (non-zero exit code), output the result
+      echo "Error: Codecov upload failed."
+      echo "Details: $result"
+      exit 1  # Exit with an error code
+  else
+      # If the command was successful, print the result
+      echo "Codecov upload succeeded."
+      echo "Details: $result"
+  fi
+fi
+
+exit ${exit_with}
